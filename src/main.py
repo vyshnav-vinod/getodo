7d5bca8c9e5d4c8cf137032fb72c293f11aeb3b4
vyshnav-vinod <vyshnav.vinod003@gmail.com>
2024-03-10 14:21:50 +0530

    Add line numbers to output file

--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
+# Currently only supports Python Files
+
 from io import TextIOWrapper
 import sys
 from os import path
@@ -15,23 +17,26 @@ def main():
     if is_dir:
         # TODO: Recursively visit every file and folder inside
         pass 
-    else:
+    else: # TODO: Check which file type and then get its corresponding comment syntax ( Prolly make a new func and check file type and call parse_TODO inside there) 
         with open(args[0]) as file:
             parse_TODO(file)
-        with open(out_file, "w+") as file:
-            file.write(out_file_contents)
+    
+    with open(out_file, "w+") as file:
+        file.write(out_file_contents)
 
 
 def parse_TODO(file: TextIOWrapper):
         file_content = file.readlines()
         global out_file_contents 
         out_file_contents = f"{file.name} : \n"
+        line_num = 0
         for line in file_content:
+            line_num += 1
             if line.startswith("#") or line.__contains__("#"):
                 line_content = line[line.index("#")+1:] # Get the contents of the comment
                 # Check if it is starts with TODO or TODO:
                 if line_content.__contains__("TODO"):
-                    out_file_contents += line_content.strip() + "\n"
+                    out_file_contents += f"Line {line_num} - {line_content.strip()}\n"
 
 
 if __name__ == "__main__":
