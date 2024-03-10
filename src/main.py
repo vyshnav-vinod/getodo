b002412e142d0f9907639469a684cc4d631644b2
vyshnav-vinod <vyshnav.vinod003@gmail.com>
2024-03-10 14:17:21 +0530

    Add TODO comment parse ability for Python files

---
+++ src/main.py
@@ -0,0 +1,39 @@
+from io import TextIOWrapper
+import sys
+from os import path
+
+
+# TODO: Add CLI args and flags
+
+out_file_contents = ""
+
+
+def main():
+    args = sys.argv[1:] # Remove the starting file name
+    is_dir = path.isdir(args[0])
+    out_file = "todo.txt" # TODO: Ask for output file from User
+    if is_dir:
+        # TODO: Recursively visit every file and folder inside
+        pass 
+    else:
+        with open(args[0]) as file:
+            parse_TODO(file)
+        with open(out_file, "w+") as file:
+            file.write(out_file_contents)
+
+
+def parse_TODO(file: TextIOWrapper):
+        file_content = file.readlines()
+        global out_file_contents 
+        out_file_contents = f"{file.name} : \n"
+        for line in file_content:
+            if line.startswith("#") or line.__contains__("#"):
+                line_content = line[line.index("#")+1:] # Get the contents of the comment
+                # Check if it is starts with TODO or TODO:
+                if line_content.__contains__("TODO"):
+                    out_file_contents += line_content.strip() + "\n"
+
+
+if __name__ == "__main__":
+    main()
+            
