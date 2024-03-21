d2c11f05aad2e40efb97bff0236481d4a91c6e5a
Vyshnav Vinod <154617184+vyshnav-vinod@users.noreply.github.com>
2024-03-21 17:40:55 +0530

    #3 (#6) Add config option for users
    * feat/config start
    
    * Add toml
    
    * Add questionary
    
    * Add interactive way to create a config file
    
    * Create a toml file and write configs to it
    
    * Add option for user to specify config in a project
    
    * Fix gitignore
    
    * Fix bug

--- src/getodo.py
+++ src/getodo.py
@@ -1,10 +1,12 @@
 # CLI functionality code
 
 
-from os import path
+from os import path, getcwd
 from todoparser import TodoParser
 
 import argparse
+import questionary
+import toml
 
 
 class ParseKeyValue(argparse.Action):
@@ -28,15 +30,19 @@ class ParseKeyValue(argparse.Action):
 
 
 def main():
+    CONFIG_FILE = ".getodo_config.toml"
     parser = argparse.ArgumentParser(
         description="Scan a file or a directory and extract all the TODO into a file/stdout"
     )
 
     # Required argument
+
     parser.add_argument("input_path", help="Directory or file to scan for TODO's")
 
     # Optional arguments
+
     parser.add_argument("-o", "--output", help="Output file to store the TODO's")
+
     parser.add_argument(
         "-t", "--term", action="store_true", help="Print the TODO's to terminal"
     )
@@ -52,6 +58,17 @@ def main():
         action=ParseKeyValue,
         help="Parse the custom filetype(s) passed as argument along with their comment syntax to parse the TODO's isnide that file",
     )
+    parser.add_argument(
+        "-c",
+        "--config",
+        action="store_true",
+        help="Add a configuration for this project",
+    )
+    parser.add_argument(
+        "--override_config",
+        action="store_true",
+        help="Do not use the custom config for this project and run getodo with its defaults",
+    )
 
     args = parser.parse_args()
 
@@ -59,7 +76,20 @@ def main():
     output_file = args.output
     print_to_terminal = args.term
     ignore_paths = args.ignore
-    add_filetypes = args.add_filetypes  # dir
+    add_filetypes = args.add_filetypes  # dict
+    config = args.config
+    use_defaults = args.override_config
+
+    if config:
+        create_config(input_path, CONFIG_FILE)
+
+    if not use_defaults:
+        config_found = config_file_present(input_path, CONFIG_FILE)
+
+        if config_found:
+            output_file, ignore_paths, add_filetypes = load_config(
+                input_path, CONFIG_FILE
+            )
 
     if ignore_paths:
         for i in range(len(ignore_paths)):
@@ -67,13 +97,86 @@ def main():
                 ignore_paths[i] = path.basename(ignore_paths[i])
             else:
                 ignore_paths[i] = path.basename(path.abspath(ignore_paths[i]))
+
     TodoParser(
         base_dir=input_path,
         out_file=output_file,
         print_to_term=print_to_terminal,
         user_ignore_paths=ignore_paths,
         user_add_filetypes=add_filetypes,
-    )
+    )  # Prolly will be put inside if use_defaults:
+
+
+def create_config(input_path, config_file_name):
+    out_file = ""
+    user_ignore_paths = []
+    user_add_filetypes = {}
+
+    out_file = questionary.text(
+        "Specify file to save the getodo output [Press ENTER for default]",
+        default="todo.txt",
+    ).ask()
+    confirm = questionary.confirm(
+        "Do you want to add files/folders to be ignored while parsing? ", default=False
+    ).ask()
+
+    while confirm:
+        user_ignore_paths.append(
+            questionary.path(
+                "Specify file/dir to ignore while parsing",
+                validate=lambda x: True if x else False,
+            ).ask()
+        )
+        confirm = questionary.confirm("Do you want to continue?", default=True).ask()
+    confirm = questionary.confirm(
+        "Do you want to add custom filetypes to be parsed? ", default=False
+    ).ask()
+
+    while confirm:
+        user_input: str = questionary.text(
+            "Specify the filetype and its comment syntax in this order [filetype,commentsyntax]",
+            validate=lambda x: True if x else False,
+        ).ask()
+        split_values = user_input.split(",")
+        user_add_filetypes[split_values[0]] = split_values[1]
+        confirm = questionary.confirm("Do you want to continue?", default=True).ask()
+
+    toml_content = {
+        "out_file": out_file,
+        "user_ignore_paths": user_ignore_paths,
+        "user_add_filetypes": user_add_filetypes,
+    }
+
+    try:
+        with open(path.join(input_path, config_file_name), "w+") as f:
+            toml.dump(toml_content, f)
+    except Exception as e:
+        print(e)
+
+    try:
+        with open(path.join(input_path, ".gitignore"), "r") as f:
+            for line in f:
+                if line == config_file_name:
+                    break
+            else:
+                with open(path.join(input_path, ".gitignore"), "a") as f:
+                    f.write(
+                        f"\n{config_file_name}"
+                    )  # Write the file name only if it is not present in .gitignore
+    except Exception as e:
+        print(e)
+
+    # TODO : Create a .getodo_config.toml in the project directory and add these into it. Also automatically add this to .gitignore
+
+
+def load_config(base_path, config_file_name):
+    config = toml.load(path.join(base_path, config_file_name))
+    return config["out_file"], config["user_ignore_paths"], config["user_add_filetypes"]
+
+
+def config_file_present(base_path, config_file_name):
+    # Will always look for the config file in the folder getodo is being called
+    return True if path.exists(path.join(base_path, config_file_name)) else False
 
 
 if __name__ == "__main__":
