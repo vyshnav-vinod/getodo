f711ddb154d9ed22498e1495b77526b49b2f89ca
Vyshnav Vinod <vyshnav.vinod003@gmail.com>
2024-04-24 11:42:09 +0530

    Add parsing and comment syntaxes

--- getodo/getodo.py
+++ getodo/getodo.py
@@ -1,32 +1,48 @@
-import os
-
+from os import path, scandir
+from json import loads
 
 class TodoParser:
 
     def __init__(self, parse_path, out_file, print_to_term) -> None:
-        
+        self._todo = ['TODO:', 'TODO :']
+        with open("comment_syntax.json", "r") as f:
+            json_data = f.read()
+        self._comments = loads(json_data)
+
         self.parse_path = parse_path
         self.out_file = out_file
         self.print_to_term = print_to_term
 
-        if os.path.isdir(parse_path):
+        if path.isdir(parse_path):
             # User provided a directory to parse
+            self.parse_dir(parse_path)
             pass
 
         else:
             # User provided a file to parse
+            self.parse_file(parse_path)
             pass
     
 
-    def parse_dir(self):
+    def parse_dir(self, dir):
         # Recursively walk the directory and when the file is encountered, parse it
-        pass
+        dir_contents = scandir(dir)
+        
+        for contents in dir_contents:
+            if contents.is_dir():
+                self.parse_dir(contents)
+            
+            if contents.is_file():
+                self.parse_file(contents)
+
 
 
     def parse_file(self, file):
         with open(file) as f:
             # Parse TODO from the file
-            pass
+            for line in f.readlines():
+                pass
+
 
 
 # TODO: After completing this, write tests before moving to next portion of the flags
